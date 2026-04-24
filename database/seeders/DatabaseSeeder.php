cfe8b1eea437061385810899a6d89f6eebf697c6
yoanafallen06 <yoanafallen@students.amikom.ac.id>
2026-04-24 07:56:26 +0700

    tambah Migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 1. Akun Admin Utama
+    \App\Models\User::create([
+    'name' => 'Admin Amikom',
+    'email' => 'admin@amikom.ac.id',
+    'password' => bcrypt('password'),
+    'role' => 'admin',
+            
         ]);
+        // 2. Insert Kategori Event
+    $category = \App\Models\Category::create([
+    'name' => 'Seminar IT',
+    'slug' => 'seminar-it',
+    ]);
+    $category2 = \App\Models\Category::firstOrCreate([
+    'name' => 'Entertaiment',
+    'slug' => 'entertaiment',
+    ]);
+    // 3. Insert Sampel Events
+    \App\Models\Event::create([
+    'category_id' => $category2->id,
+    'title' => 'Jazz Night 2025',
+    'description' => 'Nikmati malam yang indah dengan alunan musik jazz
+    yang merdu.',
+    'date' => '2026-05-10 19:00:00',
+    'location' => 'Amikom Baru',
+    'price' => 50000,
+    'stock' => 100,
+    'poster_path' => 'posters/event-1.png',
+    ]);
+    \App\Models\Event::create([
+    'category_id' => $category->id,
+    'title' => 'Hackaton - Unleash Your Inner Developer',
+    'description' => 'Ayo asah skill coding kamu dan ciptakan solusi
+    inovatif untuk tantangan masa depan!',
+    'date' => '2026-05-05 10:00:00',
+    'location' => 'Inkubator Amikom',
+    'price' => 50000,
+    'stock' => 100,
+    'poster_path' => 'posters/event-2.png',
+    ]);
+    
+    \App\Models\Event::create([
+    'category_id' => $category->id,
+    'title' => 'AI & FUTURE TECH SUMMIT 2026',
+    'description' => 'Jelajahi tren terkini dalam kecerdasan buatan dan
+    teknologi masa depan bersama para ahli di bidangnya.',
+    'date' => '2026-05-01 13:00:00',
+    'location' => 'Cinema Unit 6',
+    'price' => 50000,
+    'stock' => 100,
+    'poster_path' => 'posters/event-3.png',
+    ]);
+
     }
 }
